9e01ed7d3c2e8259f112cd787eb2bbb68ea4dab0
gaoyuze <gaoyuze>
2024-07-11 17:21:56 +0800

    new Employee

--- sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
+++ sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
@@ -7,5 +7,5 @@ public class JwtClaimsConstant {
     public static final String PHONE = "phone";
     public static final String USERNAME = "username";
     public static final String NAME = "name";
-
+    
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,14 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import net.bytebuddy.asm.Advice;
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +47,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端传过来明文密码进行md5加密处理
+       password= DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -53,5 +62,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+
+        public void save(EmployeeDTO employeeDTO){
+            System.out.println("当前线程的id:" + Thread.currentThread().getId() );
+            Employee employee = new Employee();
+
+
+            //对象属性拷贝
+            BeanUtils.copyProperties(employeeDTO,employee);
+            //设置账号状态
+            employee.setStatus(StatusConstant.ENABLE);
+            employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+            employee.setCreateTime(LocalDateTime.now());
+            employee.setUpdateTime(LocalDateTime.now());
+
+            //后期需要改为当前用户id
+            employee.setCreateUser(BaseContext.getCurrentId());
+            employee.setUpdateUser(BaseContext.getCurrentId());
+            //插入员工数据
+          employeeMapper.insert(employee);
+
+     }
 
 }
