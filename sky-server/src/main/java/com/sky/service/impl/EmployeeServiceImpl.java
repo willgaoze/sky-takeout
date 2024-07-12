438ccf4f5238a20961a2f8bce0394a99642cc265
gaoyuze <gaoyuze>
2024-07-12 16:07:40 +0800

    correct employee

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -103,10 +103,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public void startOrStop(Integer status, Long id) {
-        Employee employee=
-        Employee.builder().status(status).id(id).build();
+        Employee employee =
+                Employee.builder().status(status).id(id).build();
         employeeMapper.update(employee);
     }
 
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");
+        return employee;
 
     }
+    public void update(EmployeeDTO employeeDTO){
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO,employee);
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employeeMapper.update(employee);
+    }
+}
